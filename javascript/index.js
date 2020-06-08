ae5cdb5d54f16ccd2cde2e321f823d4d17991422
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2020-06-08 12:57:53 +0700

    added javascript

--- javascript/index.js
+++ javascript/index.js
@@ -6,7 +6,6 @@ toggle.addEventListener("click", function () {
   const items = document.querySelectorAll(".item");
 
   for (let i = 0; i < items.length; i++) {
-    console.log("item", items[i]);
     if (items[i].classList.contains("active")) {
       items[i].classList.remove("active");
       this.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
