606936c59f7513ed118aaad1978c98dafb05740e
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2020-06-08 10:11:03 +0700

    added javascript to header

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+console.log("connected!!");
+
+const toggle = document.querySelector(".toggle");
+
+toggle.addEventListener("click", function () {
+  const items = document.querySelectorAll(".item");
+
+  for (let i = 0; i < items.length; i++) {
+    console.log("item", items[i]);
+    if (items[i].classList.contains("active")) {
+      items[i].classList.remove("active");
+      this.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+      items[i].classList.add("active");
+      this.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+  }
+});
